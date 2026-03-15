fbe5d4bb8d5f11ef3295751b126a473c7948a6de
ShayanneR <srocha0327@gmail.com>
2026-03-15 12:20:28 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+
+  if (nav.classList.contains("active")) {
+    hamburger.innerHTML = "X";
+    hamburger.style.fontSize = "30px";
+  } else {
+    hamburger.innerHTML = "<span></span><span></span><span></span>";
+  }
+});
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+document.querySelectorAll('.lightbox-link').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault(); // prevent default link behavior
+    lightbox.style.display = 'flex'; // show lightbox
+    lightboxImg.src = link.href; // set image
+  });
+  });
